70bd3f062e92951ca414c1fb0bcac12506a6b4b2
truongnpt <truongnpt1998@gmail.com>
2023-02-13 22:26:27 +0700

    Add vercel file 2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      "birthday-app-alpha.vercel.app",
+    ],
+  },
 }
 
 module.exports = nextConfig
